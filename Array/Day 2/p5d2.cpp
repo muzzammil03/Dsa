25cf97698b64e93e107aa6f4452a7ed57cdc5cc1
Muzzammil Ahmed <muzzammilahmad62435@gmail.com>
2024-01-21 23:05:56 +0530

    p5d2
    triplets sum in an array

---
+++ Array/Day 2/p5d2.cpp
@@ -0,0 +1,25 @@
+//triple sum in an array
+#include <iostream>
+#include <vector>
+using namespace std;
+ 
+int main() {
+   vector<int> arr{10,20,30,40};
+   int sum=80;
+    
+    for(int i=0;i<arr.size();i++){
+        int element1=arr[i];
+        for(int j=i+1;j<arr.size();j++){
+            int element2=arr[j];
+            for(int k=j+1;k<arr.size();k++){
+                int element3=arr[k];
+                if (element1+element2+element3==sum){
+                  cout<<element1<<"+"<<element2<<"+"<<element3<<"="<<sum<<endl;
+            }
+
+            }
+        }
+    }
+    return 0;
+
+}
