0635b571715aa551f9521ef89084085764118315
Muzzammil Ahmed <muzzammilahmad62435@gmail.com>
2024-01-23 21:19:23 +0530

    p1d3
    print 2d array

---
+++ Array/Day 3/p1d3.cpp
@@ -0,0 +1,11 @@
+//2D array
+#include <iostream>
+// #include <vector
+using namespace std;
+
+int main() {
+   int arr[3][3] = {{1,2,3}, {4,5,6}, {7,8,9}};
+    cout<<arr[2][2];
+    return 0;
+
+}
